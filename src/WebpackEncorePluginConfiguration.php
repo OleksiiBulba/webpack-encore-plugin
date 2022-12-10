bc503a96e0cdeef2ca72861cd1ca8431688f7510
Oleksii Bulba <oleksii.bulba@gmail.com>
2022-12-11 00:07:44 +0300

    Initial commit

---
+++ src/WebpackEncorePluginConfiguration.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boo\MicroPlugin\WebpackEncore;
+
+use Micro\Framework\Kernel\Configuration\PluginConfiguration;
+
+class WebpackEncorePluginConfiguration extends PluginConfiguration implements WebpackEncorePluginConfigurationInterface
+{
+    const CFG_OUTPUT_PATH = 'WEBPACK_ENCORE_OUTPUT_PATH';
+
+    public function getOutputPath(): string
+    {
+        return $this->configuration->get(self::CFG_OUTPUT_PATH,
+            $this->configuration->get('BASE_PATH') . '/public/build'
+        );
+    }
+}
